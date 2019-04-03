1afb28191dc9f9367fffb5342d0482e0c141322a
maxwellgrossman <maxperryg@gmail.com>
2019-04-03 17:42:04 -0400

    need to figure out how im expected to implement multithreading

--- Assignment1.java
+++ Assignment1.java
@@ -4,11 +4,23 @@ public class Assignment1 {
 
     public static void main(String args [])
     {
-        int[] arrayToBeSorted =  new int[100];
-        for(int i = 0; i < arrayToBeSorted.length; i++){
+
+        Integer[] arrayToBeSorted =  new Integer[100];
+        fillArray(arrayToBeSorted);
+        printArray(arrayToBeSorted);
+        MergeSort<Integer> sortedArray = new MergeSort<Integer>(arrayToBeSorted);
+        sortedArray.printArray();
+
+    }
+
+    private static void fillArray(Integer[] a) {
+        for(int i = 0; i < a.length; i++){
             int r = (int) (Math.random() * 10000 + 1);
-            arrayToBeSorted[i] = r;
+            a[i] = r;
         }
-        System.out.println(Arrays.toString(arrayToBeSorted));
+    }
+
+    private static void printArray(Integer[] a){
+        System.out.println(Arrays.toString(a));
     }
 }
--- MergeSort.java
+++ MergeSort.java
@@ -1,7 +1,28 @@
+import org.omg.CORBA.Any;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public  class MergeSort<AnyType extends Comparable<? super AnyType>> {
 
+    private AnyType v [];
+    private AnyType firstHalf[];
+    private AnyType secondHalf[];
 
     public MergeSort(AnyType [] n){
+        if(n.length != 1) v = n;
+    }
 
+    public AnyType[] sort(){
+        int totalLength = v.length;
+
+        if(totalLength % 2 == 0){
+            firstHalf = (AnyType[]) Array.newInstance(v.getClass(), totalLength/2);
+            secondHalf = (AnyType[]) Array.newInstance(v.getClass(), totalLength/2);
+        }
+    }
+
+    public void printArray(){
+        System.out.println(Arrays.toString(this.v));
     }
 }
